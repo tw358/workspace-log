9ec31dfc4e22debdb70510722bda7bf4d0cf959b
tw358 <893116358@qq.com>
2023-11-30 14:04:52 +0800

    输入更新日志

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import React from 'react'
+import { useRouter } from 'next/navigation'
+import { Button } from 'antd'
 import ReactMarkdown from 'react-markdown'
 
 const textContent = `
@@ -13,17 +17,23 @@ const textContent = `
     - 新增 文件右侧栏详情页文件路径点击跳转功能
     - 优化 文件右侧栏详情页布局和详细信息的描述列表样式
     - 优化 文件右侧栏相关代码
-    - 优化 文件右侧栏详情页文件图标分辨率
     - 修复 文件右侧栏详情页文件图标宽度被压缩导致图标变形的问题
     - 修复 文件右侧栏标签页头部间距错误问题
 - 管理端
     - 新增 登录策略设置页面
-    - 新增 登录策略详情设置弹窗动态表单对登录策略ip设置增删查改功能
+    - 新增 策略表单可增加多个IP或IP段
     - 优化 使用正则对验证方法验证网段是否合法，验证是ip还是ip地址段并给出提示
     - 修复 组织同步无法进行OU和用户同步的BUG
     - 移除 移除AD用户管理中组织同步和用户同步按钮
+    
+ ## 【2023-12-08】更新日志
 `;
 
 export default function Page() {
-    return <ReactMarkdown>{textContent}</ReactMarkdown>
+    const router = useRouter()
+
+    return <div>
+        <Button type="primary" onClick={() => router.back()}>返回主页</Button>
+        <ReactMarkdown>{textContent}</ReactMarkdown>
+    </div>
 }
--- app/sitelog/page.tsx
+++ app/sitelog/page.tsx
@@ -1,4 +1,30 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+'use client'
+
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import { Button } from 'antd'
+import ReactMarkdown from 'react-markdown'
+
+const textContent = `
+# 工作日志
+        
+ ## 【2023-11-30】更新日志自检BUG
+
+- 用户端
+    - 修复 引用文件右侧栏详情页文件图标缺少引用
+    - 修复 多选文件右侧栏警告信息布局错误
+    - 修复 文件名过长时，文件右侧栏详情页会遮盖分页
+- 管理端
+    - 修复 登录策略管理列表未绑定IP/IP段，创建时间
+    - 修复 策略弹窗点击确认未调用接口并且无法关闭弹窗
+
+`;
+
 export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
+    const router = useRouter()
+
+    return <div>
+        <Button type="primary" onClick={() => router.back()}>返回主页</Button>
+        <ReactMarkdown>{textContent}</ReactMarkdown>
+    </div>
 }
