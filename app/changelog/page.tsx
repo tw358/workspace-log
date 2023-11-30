76edac75ba24a3912cf357c2274aff1a2894f7cc
tw358 <893116358@qq.com>
2023-11-30 09:56:56 +0800

    增加工作日志、更新日志页面

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,4 +1,19 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const textContent = `
+# 更新日志
+        
+ ## 【2023-12-01】更新日志
+
+- 用户端
+    - 新增  文件右侧栏详情页文件图标
+    - 新增 文件右侧栏详情页分享按钮和分享功能
+    - 新增 文件右侧栏多选文件页警告图标
+    - 优化 文件右侧栏详情页布局和详细信息的描述列表样式
+    - 修复 文件右侧栏详情页文件图标宽度被压缩导致图标变形的问题
+- 管理端`;
+
 export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
+    return <ReactMarkdown>{textContent}</ReactMarkdown>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { Button, Flex } from 'antd';
 
 export default function Page() {
     const router = useRouter()
 
     return (
-        <button type="button" onClick={() => router.push('/components')}>
-            Dashboard1
-        </button>
+        <Flex gap="small" wrap="wrap">
+            <Button type="primary"  onClick={() => router.push('/sitelog')}>工作日志</Button>
+            <Button type="primary"  onClick={() => router.push('/changelog')}>更新日志</Button>
+        </Flex>
     )
 }
